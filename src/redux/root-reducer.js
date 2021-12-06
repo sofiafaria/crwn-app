63dd69b9e4a81b06c9509cbfbaae988a5815b867
Sofia Pedroso de Faria <sofiafaria@MacBook-Pro-de-Sofia.local>
2021-12-06 20:18:06 +0000

    added cart-item and cart-icon. implemented cartreducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import CartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer'; 
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: CartReducer
 });
 
 /**
