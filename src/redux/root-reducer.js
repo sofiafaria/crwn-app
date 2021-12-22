c30ee43092a75fb894941126aecf8b370a895a97
Sofia Pedroso de Faria <sofiafaria@MacBook-Pro-de-Sofia.local>
2021-12-22 09:28:25 +0000

    Moved directory and shop data into its own reducers, created respective selectors and updated corresponding components with new redux flow for directory-shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,29 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import CartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer'; 
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
+//Note: we dont need to pass the user to the json because it is being handled by firebase
+
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: CartReducer
+    cart: CartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
 
 /**
  * Combines all the code of all the reducers using combineReducers
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -6,9 +7,11 @@ import rootReducer from './root-reducer';
 //Set up middleware
 const middlewares = [logger];
 //create store and apply all middlewares
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+//export localstorage
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
 
 /**
  * SOME NOTES ABOUT REDUX
